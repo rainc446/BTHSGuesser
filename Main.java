c56a0a16fcc473f9297ef95b7367be7e4959c1de
Rain Chen <rainc8@nycstudents.net>
2025-05-16 12:26:40 -0400

    starting gui, opens and closes

---
+++ Main.java
@@ -0,0 +1,20 @@
+import javax.swing.JFrame;
+
+public class Main {
+    //**
+    //
+    // */
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        frame.setTitle("BTHS Guesser");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(false);
+        frame.setSize(500,500);
+        frame.setVisible(true);
+
+
+
+
+    }
+}
