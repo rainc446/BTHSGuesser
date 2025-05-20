37a7ed6dca1f43df94dd23ffe2771987e9093288
rain chen <rainc8@nycstudents.net>
2025-05-20 12:24:05 -0400

    uh

--- Main.java
+++ Main.java
@@ -5,23 +5,23 @@ public class Main {
     //
     // */
 
+    private static int width = 1200;
+    private static int height = 800;
+
     public static void main(String[] args) {
         JFrame frame = new JFrame();
         frame.setTitle("BTHS Guesser");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setResizable(false);
-        frame.setSize(500,500);
+        frame.setSize(width,height); //1920, 1080
         frame.setVisible(true);
 
-
+        frame.
 
 
     }
 
-    public ImageIcon getRandomPhoto() {
-
-
-
-        return
-    }
+//    public ImageIcon getRandomPhoto() {
+//        return
+//    }
 }
