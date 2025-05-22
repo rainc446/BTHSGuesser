a5f63b1173da844d7e48ce44929818fdda9649c0
BT_1N3_08 <rainc8@Nycstudents.net>
2025-05-22 12:34:14 -0400

    e

--- Main.java
+++ Main.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class Main {
     //**
@@ -9,15 +10,24 @@ public class Main {
     private static int height = 800;
 
     public static void main(String[] args) {
+
+        /**
+         * Main Screen
+         *
+         * -first part sets up the window frame
+         *
+         * -JPanel is the display panel of the window
+         * */
         JFrame frame = new JFrame();
         frame.setTitle("BTHS Guesser");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setResizable(false);
-        frame.setSize(width,height); //1920, 1080
+        frame.setSize(width,height); //1920, 1080 is 1080p
         frame.setVisible(true);
 
-        frame.
-
+        JPanel panelOne = new JPanel();
+        panelOne.setLayout(new FlowLayout((FlowLayout.CENTER)))
+        panelOne.setBackground(Color.blue); //lowercase is mutable, capital final
 
     }
 
